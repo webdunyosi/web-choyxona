ab6c55b2ef6d533792bbae60e9d2bb26a7e66e37
Alimardon Toshpulatov <alimardontoshpulatov1978@gmail.com>
2026-02-17 08:47:38 +0500

    changed modal bg

--- src/components/ReceiptModal.jsx
+++ src/components/ReceiptModal.jsx
@@ -20,7 +20,7 @@ const ReceiptModal = ({ isOpen, onClose, orderData }) => {
 
   return (
     <div 
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4"
+      className="fixed inset-0 bg-emerald-600/40 backdrop-blur-2xl bg-opacity-50 flex items-center justify-center z-50 p-4"
       onClick={handleBackdropClick}
     >
       <div className="bg-white rounded-lg shadow-2xl max-w-md w-full max-h-[90vh] overflow-y-auto receipt-modal">
